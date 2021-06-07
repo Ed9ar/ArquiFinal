39e1b5fbec92a3feb7dd1b63286305c58f52d233
Alfredo Osuna Torres <38539148+aosunt25@users.noreply.github.com>
2021-06-07 11:21:53 -0500

    Microservicio Usuarios terminado

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'comicwire_admin'
+app.config['MYSQL_PASSWORD'] = 'AdminPassComicwire1234'
+app.config['MYSQL_DB'] = 'users_twitters'
+
+mysql = MySQL(app)
 
 @app.route("/")
 def UsuarioService():
-    return render_template('UsuarioService.html')
+
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT username FROM users")
+    data = cur.fetchall()
+    mysql.connection.commit()
+    cur.close()
+    print(data)
+    return render_template('UsuarioService.html', data=data)
 
 
 if __name__ == '__main__':
