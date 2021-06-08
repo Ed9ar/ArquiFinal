6c72a498c7e3a003682be1b3675e32fee96963c7
Alfredo Osuna Torres <38539148+aosunt25@users.noreply.github.com>
2021-06-08 17:00:48 -0500

    Servicio de tweeter separado y funcionando

--- App1/app.py
+++ App1/app.py
@@ -3,27 +3,20 @@ from flask_mysqldb import MySQL
 import pandas as pd
 import streamlit as st
 import base64
-import tweepy
-from tweepy import OAuthHandler
 import json
 from flask import jsonify
 from io import StringIO
 import csv
+import requests
 
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_USER'] = 'comicwire_admin'
+app.config['MYSQL_PASSWORD'] = 'AdminPassComicwire1234'
 app.config['MYSQL_DB'] = 'users_twitters'
 
-customer_key ='zzoPfSa0AhnSs2vtwpAShVj3P'
-customer_secret ='fZ3C0wGgAPcGvnt5N7kO3SOEFEId8Oow2wjUk4tvNRtiefHnuF'
-acces_token ='4871223373-HebkdIDGlwAXM2U6AS6vP7ZUjT212kvJPrHBEyO'
-access_token_secret ='CRbXHDCCSIfrqyHRiH3VGoTzO5vEV8ATzCGxnL7Y58vvx'
-auth = OAuthHandler(customer_key, customer_secret)
-auth.set_access_token(acces_token, access_token_secret)
-api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+
 
 
 mysql = MySQL(app)
@@ -44,20 +37,11 @@ def TwitterService():
   
     u = request.form.get("username")
     u = u.replace('@', '')
-    print("HOLA")  
-    tweets = api.user_timeline(screen_name=u, 
-                           # 200 is the maximum allowed count
-                           count=10,
-                           include_rts = False,
-                           # Necessary to keep full_text 
-                           # otherwise only the first 140 words are extracted
-                           tweet_mode = 'extended'
-                           )
     
-
-    print(tweets)
-    #data = json.loads(str(tweets))
-    return  str(tweets)
+    stringRequest = requests.get("http://127.0.0.1:8000/?username="+u)
+    tweets = json.loads(str(stringRequest.text))
+    
+    return render_template('TwitterService.html', tweets=tweets)
 
 
 
--- App2/app.py
+++ App2/app.py
@@ -1,7 +1,35 @@
-from flask import Flask
 
+from flask import Flask, render_template, request,make_response
+from flask_mysqldb import MySQL
+import tweepy
+from tweepy import OAuthHandler
+import json
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World"
+customer_key ='zzoPfSa0AhnSs2vtwpAShVj3P'
+customer_secret ='fZ3C0wGgAPcGvnt5N7kO3SOEFEId8Oow2wjUk4tvNRtiefHnuF'
+acces_token ='4871223373-HebkdIDGlwAXM2U6AS6vP7ZUjT212kvJPrHBEyO'
+access_token_secret ='CRbXHDCCSIfrqyHRiH3VGoTzO5vEV8ATzCGxnL7Y58vvx'
+auth = OAuthHandler(customer_key, customer_secret)
+auth.set_access_token(acces_token, access_token_secret)
+api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+
+@app.route('/', methods=['GET', 'POST'])
+def tweet():
+    username = request.args.get('username')
+  
+    tweets = api.user_timeline(screen_name=username, 
+                           # 200 is the maximum allowed count
+                           count=10,
+                           include_rts = False,
+                           # Necessary to keep full_text 
+                           # otherwise only the first 140 words are extracted
+                           tweet_mode = 'extended'
+                           )
+    status = tweets
+    json_str = json.dumps(status)
+
+    return  str(json_str)
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=8000, debug=True)   
